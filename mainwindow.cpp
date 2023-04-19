779e70f8e94f0f290824bf90ba01641aa5372982
HoliakaDmytro <holidim39@gmail.com>
2023-04-19 16:03:17 +0300

    Date display has been added

---
+++ mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+
+    show_date();
+
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+void MainWindow::show_date() {
+    QDate currentDate = QDate::currentDate();
+    ui->date_label->setText(currentDate.toString("dd.MM.yyyy"));
+}
+
