5af91b10aa5676fbe792316fcb98cf1599b7a35b
HoliakaDmytro <holidim39@gmail.com>
2023-04-27 16:44:40 +0300

    Tray: a program has been added to the tray

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "mainwindow.h"
 // TODO: Розбирись із видаленням динамічних об'єктів з методів і класу
 // TODO: Файл-база-даних з усією інформацією(список справ, їх стан і т.д.)
-// TODO: Перевір, щоб справа не могла бути пустою
+// (+)TODO: Перевір, щоб справа не могла бути пустою
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     MainWindow w;
     w.show();
+
     return app.exec();
 }
 
